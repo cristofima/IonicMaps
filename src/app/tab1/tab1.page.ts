2301a6cd81d64e5c1b6f977d15f389f93e13eac1
Cristopher Coronado Moreira <cristofima@hotmail.com>
2020-08-28 16:52:36 -0500

    Integrate Leaflet

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import * as L from 'leaflet';
+
+import "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/images/marker-icon.png";
+import "leaflet/dist/images/marker-icon-2x.png";
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +12,63 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  private map: L.Map;
+  private centerLatitude: number = 25.3791924;
+  private centerLongitude: number = 55.4765436;
+  private homeMarker: L.Marker;
 
+  constructor() { }
+
+  ngOnInit() {
+    this.map = L.map('map', {
+      center: [this.centerLatitude, this.centerLongitude],
+      zoom: 15,
+      renderer: L.canvas()
+    });
+
+    this.setCurrentPosition();
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '© OpenStreetMap',
+    }).addTo(this.map);
+
+    setTimeout(() => {
+      this.map.invalidateSize();
+    }, 0);
+  }
+
+  private setHomeMarker(latitude: number, longitude: number, popupText: string = null) {
+    var latLon = L.latLng(latitude, longitude);
+
+    const isFirstTime = !this.homeMarker;
+
+    if (isFirstTime) {
+      this.homeMarker = L.marker(latLon);
+    } else {
+      this.homeMarker.setLatLng(latLon);
+    }
+
+    if (popupText) {
+      this.homeMarker.bindPopup(popupText, {
+        closeButton: true
+      });
+    }
+
+    if (isFirstTime) {
+      this.homeMarker.addTo(this.map);
+    }
+  }
+
+  private setCurrentPosition() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        this.setHomeMarker(latitude, longitude, 'My location');
+
+        var latLon = L.latLng(latitude, longitude);
+        this.map.panTo(latLon);
+      });
+    }
+  }
 }
